c267876fd4f80ea7f877ac770752a801e119c657
afdavletkulov <afdavletkulov@rttv.ru>
2025-03-02 13:11:02 +0500

    fix

--- src/app/TS/tsBase.ts
+++ src/app/TS/tsBase.ts
@@ -4,15 +4,18 @@ function add(a: number, b?: number): number {
 }
 
 console.log(add(5)); // 5
-console.log(add(5, 10));  // 15
+console.log(add(5, 10)); // 15
 console.log(add(5, undefined)); /// 5
 
 //************************ */
-
+// Generics <T> - это гибкий способ работать с разными типами данных не теряя информацию о них
 //TODO: Тема: Generics (Обобщенные типы)
-const wrapInArray = <T>(arg: T): T[] => {
-  return [arg]
-}
+const wrapInArray = <T>(a: T): T[] => {
+  return [a];
+};
 //
 
-console.log(wrapInArray('df'))
+console.log(wrapInArray("df"));
+
+
+//
