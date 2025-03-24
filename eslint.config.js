a6145721689551bae57802d662d97e894f411978
Aynur <a.davletkulov@modulbank.ru>
2025-03-24 20:33:38 +0500

    test

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,12 @@ import tseslint from 'typescript-eslint'
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      ...tseslint.configs.recommended,
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+      'prettier',
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
